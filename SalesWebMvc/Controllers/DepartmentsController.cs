fa75b207c7cb4a31186107fcff50a7f0fd6404d8
Cleonildo Jr <cleu.junior@gmail.com>
2023-10-04 23:00:44 -0600

    feat: MODEL-VIEW-CONTROLLER DEpartment

---
+++ SalesWebMvc/Controllers/DepartmentsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Models;
+using System.Collections.Generic;
+
+namespace SalesWebMvc.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Department> departments = new List<Department>();
+            departments.Add(new Department { Id = 1, Name = "Eletronics" }); 
+            departments.Add(new Department { Id = 2, Name = "Fashino" });
+            departments.Add(new Department { Id = 3, Name = "Geek" });
+            departments.Add(new Department { Id = 4, Name = "Cinematic" });
+
+            return View(departments);
+        }
+    }
+}
